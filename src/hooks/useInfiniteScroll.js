ac343fc9894549e407f704303de4bc251b7a5d36
ManhThang03 <148290463+ManhThang203@users.noreply.github.com>
2026-01-01 05:21:03 +0700

    feat(infinifyLoad): add rootMargin

--- src/hooks/useInfiniteScroll.js
+++ src/hooks/useInfiniteScroll.js
@@ -9,11 +9,16 @@ export const useInfiniteScroll = ({
     if (!enabled) return;
 
     const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          onLoadMore();
+      entries.forEach(
+        (entry) => {
+          if (entry.isIntersecting) {
+            onLoadMore();
+          }
+        },
+        {
+          rootMargin: "0px 0px 200px 0px",
         }
-      });
+      );
     });
     // Do mới đâu khi với load thì current sẽ là null hoặc undefined nên dùng ? để tránh gây lỗi
     const element = targetRef?.current;
